a2a9306dc1c42900fc6019efffa857ae68a638a2
Pashasc <pasha@safaribooksonline.com>
2016-12-09 16:02:18 -0800

    add files

---
+++ _posts/2016-12-10-how-to-make-your-react-apps-15x-faster.markdown
@@ -0,0 +1,8 @@
+---
+layout: post
+title:  "What's new in webpack 2"
+date:   2016-11-10 13:15:34 -0700
+link: "https://gist.github.com/sokra/27b24881210b56bbaff7"
+---
+
+Get a glimpse of the major changes in webpack 2.
---
+++ js/main.js
@@ -0,0 +1,11 @@
+ 
+$(document).ready(function() {
+
+   /* Generate random color classes on body
+   -------------------------------------------------------------- */
+  var colors = ['blue', 'green', 'red', 'yellow'],
+    rand = Math.floor(Math.random() * colors.length);
+  
+  $('body').addClass(colors[rand],  1);
+  $('body').addClass('fade-in');
+});
